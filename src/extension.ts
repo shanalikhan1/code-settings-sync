97196b073ccfd296094121a749bb4a048d118754
Ali Arshad <ali.arshad@saphyper.com>
2024-12-06 23:34:23 +0500

    packages upgraded

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,52 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { Environment } from "./environmentPath";
+import { state } from "./state";
+import { Sync } from "./sync";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  state.context = context;
+  state.environment = new Environment();
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "code-settings-sync" is now active!');
+  const sync = new Sync();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('code-settings-sync.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from code-settings-sync!');
-	});
+  sync.bootstrap();
 
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.updateSettings",
+      (optArgument?: string) => {
+        sync.upload.bind(sync, optArgument)();
+      }
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.downloadSettings",
+      sync.download.bind(sync)
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.resetSettings",
+      sync.reset.bind(sync)
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.HowSettings",
+      sync.how.bind(sync)
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "extension.otherOptions",
+      sync.advance.bind(sync)
+    )
+  );
 }
 
 // This method is called when your extension is deactivated
