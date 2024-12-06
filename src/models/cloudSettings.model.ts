ded583576b85cef940ed8f1365ec818eda1400bf
Ali Arshad <ali.arshad@saphyper.com>
2024-12-06 22:48:23 +0500

    new project started

--- src/models/cloudSettings.model.ts
+++
@@ -1,6 +0,0 @@
-import { Environment } from "../environmentPath";
-
-export class CloudSettings {
-  public lastUpload: Date = null;
-  public extensionVersion: string = "v" + Environment.getVersion();
-}
--- test/index.ts
+++
@@ -1,8 +0,0 @@
-import * as testRunner from "vscode/lib/testrunner";
-
-testRunner.configure({
-  ui: "bdd",
-  useColors: true,
-  timeout: 5000
-});
-module.exports = testRunner;
